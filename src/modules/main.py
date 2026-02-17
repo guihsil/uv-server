4059031b99f946877212ccd61a03beb73ec2390d
guihsil <guilherme1234oliveira.go@gmail.com>
2026-02-17 14:23:50 -0300

    initial

---
+++ src/modules/main.py
@@ -0,0 +1,12 @@
+from datetime import UTC, datetime
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def home() -> dict[str, str | int]:
+    return {
+        "message": "Hello, world!",
+        "now": datetime.now(tz=UTC).isoformat(),
+        "counter": 1,
+    }
